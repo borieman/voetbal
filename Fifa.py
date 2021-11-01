4878e06dbd1b09105171a615926c015ef22a7c5c
borieman <91317867+borieman@users.noreply.github.com>
2021-11-01 13:43:46 +0100

    Update Fifa.py

--- Fifa.py
+++ Fifa.py
@@ -2,18 +2,11 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
-# import json
-# import requests
-# !pip install geopandas
-# import streamlit as st
-# from streamlit_folium import folium_static
-# import folium
-# import folium
-# import plotly.express as px
-# import plotly.graph_objects as go
-# import plotly.figure_factory as ff
-# import plotly.io as pio
-# pio.templates.default = 'seaborn'
+import plotly.express as px
+import plotly.graph_objects as go
+import plotly.figure_factory as ff
+import plotly.io as pio
+pio.templates.default = 'seaborn'
 import statsmodels.api as sm
 import seaborn as sns
 
