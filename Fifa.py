e151aed508b5082d1792a9139abb926e4e99ed01
borieman <91317867+borieman@users.noreply.github.com>
2021-11-07 18:25:48 +0100

    Update Fifa.py

--- Fifa.py
+++ Fifa.py
@@ -147,16 +147,68 @@ st.plotly_chart(plot)
 #fifa 15 lineair regressie
 
 FIFA15_23 = pd.read_csv('FIFA15LIN.csv')
-FIFA15_23['bestfit'] = sm.OLS(FIFA15_23['value_eur'],sm.add_constant(FIFA15_23['overall'])).fit().fittedvalues
-fig15=go.Figure()
-fig15.add_trace(go.Scatter(name='',x=FIFA15_23['overall'], y=FIFA15_23['value_eur'].values, mode='markers'))
-fig15.add_trace(go.Scatter(name='Lineair regressielijn', x=FIFA15_23['overall'], y=FIFA15_23['bestfit'], mode='lines'))
+FIFA22_23 = pd.read_csv('FIFA22LIN.csv')
+
+punt1 = pd.DataFrame({'overall': [93], 'value_eur': [54000000]})
+punt2 = pd.DataFrame({'overall': [93], 'value_eur': [116000000]})
+
+lin123 = go.Figure(data=[go.Scatter(
+    name = 'FIFA15',
+    x= FIFA15_23['overall'], y = FIFA15_23['value_eur'], mode='markers'
+),
+    go.Scatter(
+    name = 'FIFA22',
+    x=FIFA22_23['overall'], y = FIFA22_23['value_eur'], mode='markers'
+),
+    go.Scatter(name='Lineaire regressielijn FIFA15', x=FIFA15_23['overall'], y=FIFA15_23['bestfit'], mode='lines'
+),  
+    go.Scatter(name='Lineaire regressielijn FIFA22', x=FIFA22_23['overall'], y=FIFA22_23['bestfit'], mode='lines'
+),
+    go.Scatter(name='R2 & RSE FIFA15', x= punt1['overall'], y=punt1['value_eur'], mode='markers+text', text = "R2 = 0.728,RSE = 6450317",textposition = 'top left'
+), 
+    go.Scatter(name='R2 & RSE FIFA22', x= punt2['overall'], y=punt2['value_eur'], mode='markers+text', text = "R2 = 0.556,RSE = 19672194",textposition = 'bottom left')
 
 
-# plotly figure layout
-fig15.update_layout(title = '<b>Lineaire regressie van overall rating en de waarde van speler (FIFA15)<b>', xaxis_title = 'Overall rating', yaxis_title = 'Waarde speler in miljoenen')
+])
 
-st.plotly_chart(fig15)
+lin123.update_layout(
+    updatemenus=[
+        dict(
+            active=0,
+            buttons=list([
+                dict(label="FIFA15 & FIFA16",
+                     method="update",
+                     args=[{"visible": [True, True, True, True, True, True]},
+                          ]),
+                dict(label="FIFA15",
+                     method="update",
+                     args=[{"visible": [True, False, True, False, True, False]},
+                           ]),
+                dict(label="FIFA22",
+                     method="update",
+                     args=[{"visible": [False, True, False, True, False, True]},
+                            ]),
+                
+            ]),
+        x= 1.2,y= 0.8)
+    ])
+        
+lin123.update_layout(title='<b>Waarde van spelers</b>', title_x= 0.5,
+                  xaxis_title='Waarde van speler',
+                  yaxis_title='Aantal spelers')
+
+st.plotly_chart(lin123)
+
+# FIFA15_23['bestfit'] = sm.OLS(FIFA15_23['value_eur'],sm.add_constant(FIFA15_23['overall'])).fit().fittedvalues
+# fig15=go.Figure()
+# fig15.add_trace(go.Scatter(name='',x=FIFA15_23['overall'], y=FIFA15_23['value_eur'].values, mode='markers'))
+# fig15.add_trace(go.Scatter(name='Lineair regressielijn', x=FIFA15_23['overall'], y=FIFA15_23['bestfit'], mode='lines'))
+
+
+# # plotly figure layout
+# fig15.update_layout(title = '<b>Lineaire regressie van overall rating en de waarde van speler (FIFA15)<b>', xaxis_title = 'Overall rating', yaxis_title = 'Waarde speler in miljoenen')
+
+# st.plotly_chart(fig15)
 
 
 st.write("""
