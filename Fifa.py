cae6d6e10be8104a499a7dd70d6ca5a5c94a30e7
borieman <91317867+borieman@users.noreply.github.com>
2021-11-10 12:45:30 +0100

    Update Fifa.py

--- Fifa.py
+++ Fifa.py
@@ -119,7 +119,7 @@ if page == 'Histogram':
                        args=[{"visible": [False, True]},
                               ]),
               ]),
-          x= 1.38,y= 0.8)
+          x= 1.30,y= 0.8)
        ])
 
   plot.update_layout(title="<b>Waarde van spelers voor alle FIFA's</b>", title_x= 0.5,
@@ -180,7 +180,7 @@ if page == 'Lineaire Regressie':
                                 ]),
                 
              ]),
-         x= 1.20,y= 0.52)
+         x= 1.23,y= 0.52)
        ])
         
     lin123.update_layout(title='<b>Lineaire regressie overall rating en waarde speler FIFA15 & FIFA22</b>', title_x= 0.5,
