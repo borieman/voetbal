8b7dfc79238a3eee0e012ed4563ef4bb2605ec4c
borieman <91317867+borieman@users.noreply.github.com>
2021-11-10 12:42:17 +0100

    Update Fifa.py

--- Fifa.py
+++ Fifa.py
@@ -119,7 +119,7 @@ if page == 'Histogram':
                        args=[{"visible": [False, True]},
                               ]),
               ]),
-          x= 1.35,y= 0.8)
+          x= 1.38,y= 0.8)
        ])
 
   plot.update_layout(title="<b>Waarde van spelers voor alle FIFA's</b>", title_x= 0.5,
