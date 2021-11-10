66108c8b98152684879fd986b14bb549a6081a28
borieman <91317867+borieman@users.noreply.github.com>
2021-11-10 12:01:19 +0100

    Update Fifa.py

--- Fifa.py
+++ Fifa.py
@@ -91,25 +91,25 @@ if page == 'Histogram':
   ])
 
   plot.update_layout(
-     updatemenus=[
-          dict(
-              active=0,
-              buttons=list([
-                  dict(label="Overall en Potential",
-                       method="update",
-                       args=[{"visible": [True, True]},
-                             ]),
-                  dict(label="Overall",
-                       method="update",
-                       args=[{"visible": [True, False]},
-                             ]),
-                  dict(label="Potential",
-                       method="update",
-                      args=[{"visible": [False, True]},
-                            ]),
-              ]),
-           x= 1.43,y= 0.8)
-     ])
+       updatemenus=[
+             dict(
+                  active=0,
+                 buttons=list([
+                      dict(label="Overall en Potential",
+                          method="update",
+                        args=[{"visible": [True, True]},
+                                 ]),
+                      dict(label="Overall",
+                        method="update",
+                           args=[{"visible": [True, False]},
+                                 ]),
+                      dict(label="Potential",
+                           method="update",
+                          args=[{"visible": [False, True]},
+                                ]),
+               ]),
+            x= 1.43,y= 0.8)
+         ])
 
     plot.update_layout(title="<b>Waarde van spelers voor alle FIFA's</b>", title_x= 0.5,
                     xaxis_title='Waarde van speler',
